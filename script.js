04be5060549d6239c27c1ff536b5b6ef0db67429
CodeurIvoirien225 <koneelabass1@gmail.com>
2025-03-20 05:56:45 +0000

    Ajout du fichier js

---
+++ script.js
@@ -0,0 +1,71 @@
+
+/* projets */
+document.getElementById("voirProjets").addEventListener("click", function(event) {
+    event.preventDefault(); // Empêcher le comportement par défaut du lien
+
+    // Sélectionner la section cible
+    const section = document.getElementById("projects");
+    
+
+    // Faire défiler la page en douceur jusqu'à la section
+    section.scrollIntoView({ behavior: "smooth" });
+});
+
+
+/* projets */
+document.getElementById("voirprojets").addEventListener("click", function(event) {
+    event.preventDefault(); // Empêcher le comportement par défaut du lien
+
+    // Sélectionner la section cible
+    const section = document.getElementById("projects");
+    
+
+    // Faire défiler la page en douceur jusqu'à la section
+    section.scrollIntoView({ behavior: "smooth" });
+});
+
+
+/* home */
+document.getElementById("accueil").addEventListener("click", function(event) {
+    event.preventDefault(); // Empêcher le comportement par défaut du lien
+
+    // Sélectionner la section cible
+    const section = document.getElementById("home");
+
+    // Faire défiler la page en douceur jusqu'à la section
+    section.scrollIntoView({ behavior: "smooth" });
+});
+
+
+/* skills */
+document.getElementById("skill").addEventListener("click", function(event) {
+    event.preventDefault(); // Empêcher le comportement par défaut du lien
+
+    // Sélectionner la section cible
+    const section = document.getElementById("skills");
+
+    // Faire défiler la page en douceur jusqu'à la section
+    section.scrollIntoView({ behavior: "smooth" });
+});
+
+/* about */
+document.getElementById("abouts").addEventListener("click", function(event) {
+    event.preventDefault(); // Empêcher le comportement par défaut du lien
+
+    // Sélectionner la section cible
+    const section = document.getElementById("about");
+
+    // Faire défiler la page en douceur jusqu'à la section
+    section.scrollIntoView({ behavior: "smooth" });
+});
+
+/* contact */
+document.getElementById("contacts").addEventListener("click", function(event) {
+    event.preventDefault(); // Empêcher le comportement par défaut du lien
+
+    // Sélectionner la section cible
+    const section = document.getElementById("contact");
+
+    // Faire défiler la page en douceur jusqu'à la section
+    section.scrollIntoView({ behavior: "smooth" });
+});
